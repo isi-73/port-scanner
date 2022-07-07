ba1404684639821e842ffe0b3f5772b9319d048b
isi-73 <108881767+isi-73@users.noreply.github.com>
2022-07-07 16:22:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import socket
+import time
+import threading
+from queue import Queue
+
+socket.setdefaulttimeout(0.25)
+lock = threading.Lock()
+
+ip_address = input('IP Address/Hostname: ')
+host = socket.gethostbyname(ip_address)
+print ('Scanning on IP Address: ', host)
+
+def scan(port):
+   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      con = sock.connect((host, port))
+      with lock:
+         print('Port:', port)
+      con.close()
+   except:
+      pass
+
+def execute():
+   while True:
+      worker = queue.get()
+      scan(worker)
+      queue.task_done()
+      
+queue = Queue()
+start_time = time.time()
+   
+for x in range(100):
+   thread = threading.Thread(target = execute)
+   thread.daemon = True
+   thread.start()
+   
+for worker in range(1, 500):
+   queue.put(worker)
+   
+queue.join()
+
+print('Scan taked:', time.time() - start_time,'Seconds')
